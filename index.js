22fb5b98d7c654e4a70e9ecd6d01ca36bafe605f
rrrossettiii <rrrossettiii@gmail.com>
2020-12-04 05:15:18 -0500

    refactored a bit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Required Packages - inquirer; axios; file system;
-const inquirer = require("inquirer")
-const axios = require("axios")
-const fs = require("fs")
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
 
 // Utilities - generateMarkdown.js;
-const generateMarkdown = require("./utils/generateMarkdown.js")
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // API Arrays - (DEFAULT)
 const userGitHub = [];
@@ -13,124 +13,153 @@ const userRepositories = [];
 // User Info;
 // - axios + GitHub api
 const axiosJS = {
-    async getUser(usernameGH) {
-        try {
-            // - pulls user data; pulls user's repository data;
-            userGitHub.push((await axios.get(`https://api.github.com/users/${usernameGH}`)).data);
-            userRepositories.push(...(await axios.get(`https://api.github.com/users/${usernameGH}/repos?per_page=100`)).data.map(repo => repo.name));
-        } 
-        catch {
-            console.log(`\nUsername not found.`);
-        }
-    }
+	async getUser(usernameGH) {
+		try {
+			// - pulls user data; pulls user's repository data;
+			userGitHub.push(
+				(await axios.get(`https://api.github.com/users/${usernameGH}`)).data
+			);
+			userRepositories.push(
+				...(
+					await axios.get(
+						`https://api.github.com/users/${usernameGH}/repos?per_page=100`
+					)
+				).data.map(repo => repo.name)
+			);
+		} catch {
+			console.log(`\nUsername not found.`);
+		}
+	}
 };
-
 
 // Inquirer Prompts;
 const userQuestions = [
-    // - Username; - REQUIRED;
-    {
-        name: 'Username',
-        type: 'input',
-        message: 'Enter your GitHub Username: ',
-        transformer: function(a,b) {
-            return `${'@' + a}`
-        },
-        validate: async usernameGH => {
-            // - test username against GitHub;
-            await axiosJS.getUser(usernameGH);
-            if (userRepositories.length < 1){
-                userGitHub.splice(0, userGitHub.length);
-                return console.log('\nA valid GitHub Username is required to proceed.');
-        }
-        return true;}
-    },
-    // - Repository; - REQUIRED;
-    {
-        name: 'Repository',
-        type: 'list',
-        message: 'Select your GitHub Repository:',
-        choices: userRepositories
-    },
-    // - Title; - REQUIRED;
-    {
-        name: 'Title',
-        type: 'input',
-        default: 'Project_Title',
-        message: 'Enter the TITLE of your project:',
-        validate: (response) => {if (response.length < 1){
-            return console.log('\n\nPlease title your project.');
-        }
-        return true;}
-    },
-    // - Description; - REQUIRED;
-    {
-        name: 'Description',
-        type: 'input',
-        message: 'Please write a DESCRIPTION of your project:',
-        validate: (response) => {
-            if (response.length < 1){return console.log('\n\nA valid description is required to proceed.');
-        }
-        return true;}
-    },
-    // - Installation; - OPTIONAL;
-    {
-        name: 'Installation',
-        type: 'input',
-        message: "(OPTIONAL) - Describe the steps required to INSTALL your project:",
-        transformer: (input) => {
-            splitInput = input.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
-            numberList = splitInput.map((arr, index) => `${index + 1}.\t` + arr);
-            listFormat = numberList.join('\n');
-            return '*Use sentences with punctuation/Capital letters*' + '\n' + listFormat + '<<<';
-        }
-    },
-    // - Usage; - OPTIONAL;
-    {
-        name: 'Usage',
-        type: 'input',
-        message: "(OPTIONAL) - Enter instructions and examples of your project in USE:",
-    },
-    // - Contributions; - OPTIONAL;
-    {
-        name: 'Contributions',
-        type: 'input',
-        message: "(OPTIONAL) - Enter guidelines on how other developers can CONTRIBUTE to your project:",
-    },
-    // - Testing; - OPTIONAL;
-    {
-        name: 'Testing',
-        type: 'input',
-        message: "(OPTIONAL) - Describe the TESTS for your application and examples on how to run them:",
-    },
-    // - License; - REQUIRED;
-    {
-        name: 'License',
-        type: 'list',
-        message: "Choose a license for your project:",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
-    }
+	// - Username; - REQUIRED;
+	{
+		name: "Username",
+		type: "input",
+		message: "Enter your GitHub Username: ",
+		transformer: function (a, b) {
+			return `${"@" + a}`;
+		},
+		validate: async usernameGH => {
+			// - test username against GitHub;
+			await axiosJS.getUser(usernameGH);
+			if (userRepositories.length < 1) {
+				userGitHub.splice(0, userGitHub.length);
+				return "A valid GitHub Username is required to proceed.";
+			}
+			return true;
+		}
+	},
+	// - Repository; - REQUIRED;
+	{
+		name: "Repository",
+		type: "list",
+		message: "Select your GitHub Repository:",
+		choices: userRepositories
+	},
+	// - Title; - REQUIRED;
+	{
+		name: "Title",
+		type: "input",
+		default: "Project_Title",
+		message: "Enter the TITLE of your project:",
+		validate: response => {
+			if (response.length < 1) {
+				return "Please title your project.";
+			}
+			return true;
+		}
+	},
+	// - Description; - REQUIRED;
+	{
+		name: "Description",
+		type: "input",
+		message: "Please write a DESCRIPTION of your project:",
+		validate: response => {
+			if (response.length < 1) {
+				return "A valid description is required to proceed.";
+			}
+			return true;
+		}
+	},
+	// - Installation; - OPTIONAL;
+	{
+		name: "Installation",
+		type: "input",
+		message:
+			"(OPTIONAL) - Describe the steps required to INSTALL your project:",
+		transformer: input => {
+			splitInput = input.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|");
+			numberList = splitInput.map((arr, index) => `${index + 1}.\t` + arr);
+			listFormat = numberList.join("\n");
+			return (
+				"*Use sentences with punctuation/Capital letters*" +
+				"\n" +
+				listFormat +
+				"<<<"
+			);
+		}
+	},
+	// - Usage; - OPTIONAL;
+	{
+		name: "Usage",
+		type: "input",
+		message:
+			"(OPTIONAL) - Enter instructions and examples of your project in USE:"
+	},
+	// - Contributions; - OPTIONAL;
+	{
+		name: "Contributions",
+		type: "input",
+		message:
+			"(OPTIONAL) - Enter guidelines on how other developers can CONTRIBUTE to your project:"
+	},
+	// - Testing; - OPTIONAL;
+	{
+		name: "Testing",
+		type: "input",
+		message:
+			"(OPTIONAL) - Describe the TESTS for your application and examples on how to run them:"
+	},
+	// - License; - REQUIRED;
+	{
+		name: "License",
+		type: "list",
+		message: "Choose a license for your project:",
+		choices: [
+			"GNU AGPLv3",
+			"GNU GPLv3",
+			"GNU LGPLv3",
+			"Mozilla Public License 2.0",
+			"Apache License 2.0",
+			"MIT License",
+			"Boost Software License 1.0",
+			"The Unlicense"
+		]
+	}
 ];
 
 // Wite README.md;
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
-        if(err){
-            return console.log(err);
-        }
-        console.log('\nYour README.md has been generated.');
-    })
+	fs.writeFile(fileName, data, err => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log("\nYour README.md has been generated.");
+	});
 }
 
 // Initialize Program Function;
 async function init() {
-    // Prompt Questions;
-    const userResponses = await inquirer.prompt(userQuestions) 
-    console.log('\n\nYour Responses: ', userResponses);
+	// Prompt Questions;
+	const userResponses = await inquirer.prompt(userQuestions);
+	console.log("\n\nYour Responses: ", userResponses);
 
-    // Generate README; - generate markdown; write file;
-    const userReadme = generateMarkdown(userResponses, userGitHub[0])
-    await writeToFile('README.md', userReadme)
+	// Generate README; - generate markdown; write file;
+	const userReadme = generateMarkdown(userResponses, userGitHub[0]);
+	await writeToFile("./Generated README/README.md", userReadme);
 }
 
 // CALL - Initialize Program;
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,76 +2,88 @@
 // - '\n' = line break;  '\n&nbsp;\n' = empty line (in Markdown);
 
 function generateMarkdown(userResponses, userGitHub) {
-  // Readme Header (Default);
-  let readmeDraft =
-  // - title; badges; *empty space*; description; *more empty space*
-  `# ${userResponses.Title}
+	// Readme Header (Default);
+	let readmeDraft =
+		// - title; badges; *empty space*; description; *more empty space*
+		`# ${userResponses.Title}
   \n${userResponses.Repository}
-  \n![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.Username}/${userResponses.Repository}?style=flat&logo=appveyor)![Badge for License](https://img.shields.io/github/license/${userResponses.Username}/${userResponses.Repository}?style=flat&logo=appveyor)
+  \n![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponses.Username}/${userResponses.Repository}?style=flat&logo=appveyor)![Badge for Top Language](https://img.shields.io/github/languages/top/${userResponses.Username}/${userResponses.Repository}?style=flat&logo=appveyor)
   \n&nbsp;\n \n&nbsp;\n
   \n## Description:
   \n${userResponses.Description}
-  \n&nbsp;\n \n&nbsp;\n`
-  
-  // Readme Footer (Default); - license;
-  let readmeFooter = `\n## License: \n${userResponses.License}
   \n&nbsp;\n \n&nbsp;\n`;
-  
-  // Table of Contents || TOC (Default); - TOC; optional entries;
-  let tableOfContents = `\n## Table of Contents \n* [License](#license)`;
-  
-  // - Installation - if response is not empty or 'undefined'...;
-  if (userResponses.Installation !== ('' || 'use sentences with punctuation/capital letters')){
-    // - Format - split to array of sentences; number sentences; convert back to string;
-    instructionsArray = userResponses.Installation.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")
-    numberedInstructionsArray = instructionsArray.map((arr, index) => `${index + 1}.\t` + arr);
-    userInstructions = numberedInstructionsArray.join(" \n")
-    // - add entry to TOC; add content to Footer;
-    tableOfContents += `\n* [Installation](#installation)`;
-    readmeFooter += `\n## Installation: \n${userInstructions}
-    \n&nbsp;\n \n&nbsp;\n`;}
 
-  // - Usage - etc...;
-  if (userResponses.Usage.length > 1){
-    console.log(userResponses.Usage);
-    console.log('test');
-    tableOfContents += `\n* [Usage](#usage)`;
-    readmeFooter += `\n## Usage: \n${userResponses.Usage}
+	// Readme Footer (Default); - license;
+	let readmeFooter = `\n## License: \n${userResponses.License}
+  \n&nbsp;\n \n&nbsp;\n`;
+
+	// Table of Contents || TOC (Default); - TOC; optional entries;
+	let tableOfContents = `\n## Table of Contents \n* [License](#license)`;
+
+	// - Installation - if response is not empty or 'undefined'...;
+	if (
+		userResponses.Installation !==
+		("" || "use sentences with punctuation/capital letters")
+	) {
+		// - Format - split to array of sentences; number sentences; convert back to string;
+		instructionsArray = userResponses.Installation.replace(
+			/([.?!])\s*(?=[A-Z])/g,
+			"$1|"
+		).split("|");
+		numberedInstructionsArray = instructionsArray.map(
+			(arr, index) => `${index + 1}.\t` + arr
+		);
+		userInstructions = numberedInstructionsArray.join(" \n");
+		// - add entry to TOC; add content to Footer;
+		tableOfContents += `\n* [Installation](#installation)`;
+		readmeFooter += `\n## Installation: \n${userInstructions}
+    \n&nbsp;\n \n&nbsp;\n`;
+	}
+
+	// - Usage - etc...;
+	if (userResponses.Usage.length > 1) {
+		console.log(userResponses.Usage);
+		console.log("test");
+		tableOfContents += `\n* [Usage](#usage)`;
+		readmeFooter += `\n## Usage: \n${userResponses.Usage}
     \n<img alt="Usage Capture" src="assets/usageCapture.gif" width="50%" height="50%"/>
-    \n&nbsp;\n \n&nbsp;\n`;}
+    \n&nbsp;\n \n&nbsp;\n`;
+	}
 
-  // - Contributions - etc...;
-  if (userResponses.Contributions.length > 1){
-    tableOfContents += `\n* [Contributions](#contributions)`;
-    readmeFooter += `\n## Contributions: \n${userResponses.Contributions}
-    \n&nbsp;\n \n&nbsp;\n`;}
+	// - Contributions - etc...;
+	if (userResponses.Contributions.length > 1) {
+		tableOfContents += `\n* [Contributions](#contributions)`;
+		readmeFooter += `\n## Contributions: \n${userResponses.Contributions}
+    \n&nbsp;\n \n&nbsp;\n`;
+	}
 
-  // - Testing - etc...;
-  if (userResponses.Testing.length > 1){
-    tableOfContents += `\n* [Testing](#testing)`;
-    readmeFooter += `\n## Testing: \n${userResponses.Testing}
-    \n&nbsp;\n \n&nbsp;\n`;}
+	// - Testing - etc...;
+	if (userResponses.Testing.length > 1) {
+		tableOfContents += `\n* [Testing](#testing)`;
+		readmeFooter += `\n## Testing: \n${userResponses.Testing}
+    \n&nbsp;\n \n&nbsp;\n`;
+	}
 
-  // Questions; - add questions to Table of Contents & Footer;
-  tableOfContents += `\n* [Questions](#questions)
-  \n&nbsp;\n \n&nbsp;\n`
+	// Questions; - add questions to Table of Contents & Footer;
+	tableOfContents += `\n* [Questions](#questions)
+  \n&nbsp;\n \n&nbsp;\n`;
 
-  // - Contact Info
-  readmeFooter += `\n## Questions?
+	// - Contact Info
+	readmeFooter += `\n## Questions?
   \nIf you have any questions, contact me with the information below:
   \n[![Profile Picture](${userGitHub.avatar_url})](${userGitHub.url})
-  \nGitHub Username: [@${userGitHub.login}](${userGitHub.url})`
+  \nGitHub Username: [@${userGitHub.login}](${userGitHub.url})`;
 
-  // Email?
-  if (userGitHub.email !== null){
-    readmeFooter += `\nEmail: ${userGitHub.email}`;
-} 
+	// Email?
+	if (userGitHub.email !== null) {
+		readmeFooter += `\nEmail: ${userGitHub.email}`;
+	}
 
-// Assemble Draft; - add TOC to draft; add footer to draft.
-readmeDraft += tableOfContents;
-readmeDraft += readmeFooter;
+	// Assemble Draft; - add TOC to draft; add footer to draft.
+	readmeDraft += tableOfContents;
+	readmeDraft += readmeFooter;
 
-// Return and Export;
-return readmeDraft;
+	// Return and Export;
+	return readmeDraft;
 }
 module.exports = generateMarkdown;
